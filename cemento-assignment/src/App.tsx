2d53004653dd608396664804fb157c27ba6209ce
Ofek Shpirer <ofekshp2821@gmail.com>
2025-06-25 14:36:36 +0300

    react typescript tailwind

---
+++ cemento-assignment/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind is working! 🎉</h1>
+      <p className="mt-4 text-lg text-gray-600">Let's build this assignment!</p>
+    </div>
+  );
+}
+
+export default App;
